977c7bbac18b3643f8f66f6210172bf4a2caa32d
Ritesh <dahiteritesh@gmail.com>
2023-02-02 15:26:02 +0530

    Create java file with linear search code and call commit.

---
+++ LinearSearch.java
@@ -0,0 +1,21 @@
+public class LinearSearch {
+    public static int search(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5 };
+        int target = 4;
+        int index = search(array, target);
+        if (index != -1) {
+            System.out.println("Element found at index " + index);
+        } else {
+            System.out.println("Element not found in the array");
+        }
+    }
+}
